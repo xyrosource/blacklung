a975700684a90a97d9cedd75aeff4d74783e21bf
Michael Leandersson <tripokey@gmail.com>
2017-04-13 18:30:32 +0200

    Fixed lint deps

--- ci.sh
+++ ci.sh
@@ -3,9 +3,12 @@ set -e
 
 PATH="$HOME"/.cargo/bin:$PATH
 
-git clone https://github.com/mozilla/nixpkgs-mozilla
-pushd nixpkgs-mozilla
-./rust-overlay-install.sh
-popd
+if ! [[ -f "$HOME/.config/nixpkgs/overlays/rust-overlay.nix" ]]
+then
+    git clone https://github.com/mozilla/nixpkgs-mozilla
+    pushd nixpkgs-mozilla
+    ./rust-overlay-install.sh
+    popd
+fi
 
-nix-shell -p rustChannels.nightly.rust --run 'cargo install --force clippy; cargo clean; cargo clippy'
+nix-shell --pure -p rustChannels.nightly.rust gcc --run 'cargo install --force clippy; cargo clean; cargo clippy'
