a4c36311f2d45b1c5c7358ba6c59013be9531cdb
Michael Leandersson <tripokey@gmail.com>
2017-04-13 19:56:44 +0200

    Build and test in ci

--- ci.sh
+++ ci.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PATH="$HOME"/.cargo/bin:$PATH
-
+# Install overlay
 if ! [[ -f "$HOME/.config/nixpkgs/overlays/rust-overlay.nix" ]]
 then
     git clone https://github.com/mozilla/nixpkgs-mozilla
@@ -11,6 +10,10 @@ then
     popd
 fi
 
+# Build and test on stable
+nix-shell --pure -p rustChannels.stable.rust gcc --run "cargo clean && cargo build && cargo test"
+
+# Lint with clippy
 if [[ $# -eq 1 ]] && [[ "$1" = "-n" ]]
 then
   INSTALL=""
@@ -18,4 +21,5 @@ else
   INSTALL="cargo install --force clippy && "
 fi
 
+PATH="$HOME"/.cargo/bin:$PATH
 nix-shell --pure -p rustChannels.nightly.rust gcc --run "$INSTALL cargo clean && cargo clippy -- -D warnings"
