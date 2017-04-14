f5d355e2fdc46e6a903bbbe4e306e1705449957b
Michael Leandersson <tripokey@gmail.com>
2017-04-14 15:11:56 +0200

    shell.nix uses nixpkgs-mozilla

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 with import <nixpkgs> { };
 
+let
+  rust = (rustChannels.stable.rust.override { extensions = [ "rust-src" ]; });
+in
 stdenv.mkDerivation {
   name = "blacklung";
+  buildInputs = [ rust rustracer rustfmt gcc ];
 
-  buildInputs = [ rustc cargo rustfmt ];
+  RUST_SRC_PATH= "${rust}/lib/rustlib/src/rust/src";
 }
